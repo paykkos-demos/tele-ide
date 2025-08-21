a9608711272cf9724711dfa65cdde06f76d28c34
paykko <paykko@protonmail.com>
2025-08-21 23:30:27 +0200

    display terminal charachter map size and cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,36 @@
 import React from 'react';
-import { render, Box, Text } from 'ink';
+import { render, Box, Text, useStdout } from 'ink';
 
 const App = () => {
-  return (
-    <Box flexDirection="row" height="100%">
-      {/* Left panel: Tree view */}
-      <Box
-        width={30}
-        borderStyle="round"
-        borderColor="cyan"
-        padding={1}
-        flexDirection="column"
-      >
-        <Text color="cyan">Tree View</Text>
-      </Box>
+	const { stdout } = useStdout();
 
-      {/* Right panel: File view */}
-      <Box
-        flexGrow={1}
-        borderStyle="round"
-        borderColor="green"
-        padding={1}
-        flexDirection="column"
-      >
-        <Text color="green">File View</Text>
-      </Box>
-    </Box>
-  );
+	return (
+		<Box flexDirection="row" flexGrow={1} height="100%">
+			<Box
+				width={30}
+				minHeight={0}
+				borderStyle="round"
+				borderColor="cyan"
+				padding={1}
+				flexDirection="column"
+			>
+				<Text color="cyan">Tree View</Text>
+			</Box>
+
+			<Box
+				flexGrow={1}
+				borderStyle="round"
+				borderColor="green"
+				padding={1}
+				flexDirection="column"
+			>
+				<Text color="green">File View</Text>
+				<Text>
+					Terminal size: {stdout.columns}x{stdout.rows}
+				</Text>
+			</Box>
+		</Box>
+	);
 };
 
 render(<App />);
